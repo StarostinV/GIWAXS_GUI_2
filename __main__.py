664ad438458ff5d7d67cac6d1afa2200c8daf3a1
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-06-05 09:13:04 +0200

    minor refactoring

--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
 import sys
-from pathlib import Path
 import logging
 
 from PyQt5.QtWidgets import QApplication
@@ -14,9 +13,4 @@ if __name__ == '__main__':
 	qapp = QApplication(sys.argv)
 	giwaxs_app = App()
 	window = DockAreaWidget()
-	# giwaxs_app.fm.open_project(Path(__file__).parent / 'test_project')
-	# data_folder = Path(__file__).parent / 'data'
-	# giwaxs_app.fm._project_structure.new_time_series('_real_time', sorted(list(data_folder.glob('*.tiff'))))
-
-	# giwaxs_app.roi_dict.create_roi(10, 10)
 	sys.exit(qapp.exec_())
