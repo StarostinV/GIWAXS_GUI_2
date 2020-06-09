e81fa36c9829990df4c87e616927964fc5708235
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-06-09 09:48:35 +0200

    InternalError exception added

--- giwaxs_gui/app/utils.py
+++ giwaxs_gui/app/utils.py
@@ -1,8 +1,11 @@
 class SingletonMeta(type):
-
     _instance = None
 
     def __call__(cls):
         if cls._instance is None:
             cls._instance = super().__call__()
         return cls._instance
+
+
+class InternalError(Exception):
+    pass
