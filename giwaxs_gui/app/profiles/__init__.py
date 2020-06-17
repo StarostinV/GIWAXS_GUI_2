0344df8eec154b0003094d4bd11ac2f714fcf22b
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-06-17 18:22:32 +0200

    import SavedProfile

--- giwaxs_gui/app/profiles/__init__.py
+++ giwaxs_gui/app/profiles/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .basic_profile import BasicProfile, BaselineParams
+from .basic_profile import BasicProfile, BaselineParams, SavedProfile
 from .radial_profile import RadialProfile
 from .angular_profile import AngularProfile
