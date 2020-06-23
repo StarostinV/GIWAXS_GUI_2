a77e9af9857ed31a41ecbf41e4f7e3e2066d7c1a
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-06-23 16:27:45 +0200

    scroll area for recent projects

--- giwaxs_gui/gui/init_window.py
+++ giwaxs_gui/gui/init_window.py
@@ -7,10 +7,10 @@ from pathlib import Path
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QGridLayout,
                              QVBoxLayout, QFileDialog, QLabel,
-                             QLineEdit)
+                             QLineEdit, QScrollArea, QSizePolicy)
 from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
 
-from .tools import center_widget, show_error, Icon, color_animation
+from .tools import center_widget, show_error, Icon, color_animation, get_folder_filepath
 from .basic_widgets import Label
 from ..__version import __version__
 
@@ -20,6 +20,9 @@ class InitWindow(QWidget):
     sigExit = pyqtSignal()
 
     log = logging.getLogger(__name__)
+
+    WIDTH = 600
+    HEIGHT = 400
 
     def __init__(self,
                  recent_projects: List[Path] = None,
@@ -35,8 +38,8 @@ class InitWindow(QWidget):
         self.project_path: Path = Path('~/GIWAXS projects').expanduser().resolve() / self.project_name
 
         self.__init_ui(recent_projects, is_updated)
-        self.setFixedWidth(600)
-        self.setFixedHeight(400)
+        self.setFixedWidth(self.WIDTH)
+        self.setFixedHeight(self.HEIGHT)
         center_widget(self)
         self.show()
 
@@ -63,16 +66,7 @@ class InitWindow(QWidget):
         layout.addWidget(self.create_button, 2, 2)
 
         if recent_projects:
-            r_layout = QVBoxLayout()
-            r_layout.addWidget(QLabel('', self))
-            r_layout.addWidget(Label('Recent projects', self, 9))
-            for path in recent_projects:
-                btn = QPushButton(path.name)
-                btn.clicked.connect(
-                    lambda *x, p=path: self.sigOpenProject.emit(p))
-                r_layout.addWidget(btn)
-            layout.addLayout(r_layout, 3, 0, 1, 3)
-
+            layout.addWidget(self._init_recent_projects_area(recent_projects),  3, 0, 1, 3)
         e_layout = QVBoxLayout()
 
         exit_button = QPushButton('Exit')
@@ -80,7 +74,29 @@ class InitWindow(QWidget):
         e_layout.addWidget(QLabel(''))
         e_layout.addWidget(exit_button)
 
-        layout.addLayout(e_layout, 4, 0, 1, 3)
+        layout.addLayout(e_layout, 4 if recent_projects else 3, 0, 1, 3)
+
+    def _init_recent_projects_area(self, recent_projects):
+        q_scroll_area = QScrollArea(self)
+        q_scroll_area.setWidgetResizable(True)
+        q_scroll_area.setGeometry(0, 0, self.WIDTH, self.HEIGHT // 2)
+
+        projects_list_widget = QWidget(self)
+        projects_list_widget.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
+
+        scroll_layout = QVBoxLayout(projects_list_widget)
+        scroll_layout.addWidget(QLabel('', self))
+        scroll_layout.addWidget(Label('Recent projects', self, 9))
+        for path in recent_projects:
+            btn = QPushButton(path.name)
+            btn.clicked.connect(
+                lambda *x, p=path: self.sigOpenProject.emit(p))
+            scroll_layout.addWidget(btn)
+        q_scroll_area.setWidget(projects_list_widget)
+
+        return q_scroll_area
 
     @pyqtSlot(str, name='onTextEditing')
     def _on_text_editing(self, path: str):
@@ -112,5 +128,8 @@ class InitWindow(QWidget):
         except FileExistsError:
             show_error(f'The folder {str(self.project_path.resolve())} already exists', error_title='Folder exists')
             color_animation(self.file_line)
-        except Exception as err:
-            self.log.exception(err)
+        except PermissionError:
+            folder = get_folder_filepath(self, 'Permission denied, please, create a project folder manually',
+                                         directory=str(Path('~').expanduser().resolve()))
+            if folder and folder.is_dir():
+                self.sigOpenProject.emit(folder.resolve())
