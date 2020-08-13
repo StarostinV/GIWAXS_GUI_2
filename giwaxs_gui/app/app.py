87be08a99c36b6a7f6050e527e07b147dd6e086a
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-08-13 13:47:09 +0200

    added data_manager and current_image_key

--- giwaxs_gui/app/app.py
+++ giwaxs_gui/app/app.py
@@ -1,39 +1,34 @@
 import logging
+from pathlib import Path
 
 from .rois.roi_dict import RoiDict
-from .file_manager import FileManager
+from .file_manager import FileManager, ImageKey
 from .utils import SingletonMeta
 from .geometry import Geometry
 from .geometry_holder import GeometryHolder
 from .image_holder import ImageHolder
 from .profiles import RadialProfile, AngularProfile
 from .debug_tracker import TrackQObjects
+from .data_manager import DataManager
 
 
 class App(metaclass=SingletonMeta):
     log = logging.getLogger(__name__)
 
-    def __init__(self):
-        self.fm: FileManager = FileManager()
+    def __init__(self, config_path: Path = None):
+        self.fm: FileManager = FileManager(config_path)
         self.geometry_holder: GeometryHolder = GeometryHolder(self.fm)
         self.roi_dict: RoiDict = RoiDict(self.fm, self.geometry_holder)
         self.image_holder: ImageHolder = ImageHolder(self.fm, self.geometry_holder, self.roi_dict)
         self.radial_profile: RadialProfile = RadialProfile(self.image_holder, self.fm)
         self.angular_profile: AngularProfile = AngularProfile(self.image_holder)
+        self.data_manager: DataManager = DataManager(self.fm, self.image_holder)
 
-        self.image_holder.sigPolarImageChanged.connect(self.radial_profile.update)
-        self.image_holder.sigPolarImageChanged.connect(self.angular_profile.update)
+        self._connect_app()
 
-        self.geometry_holder.sigScaleChanged.connect(self.roi_dict.on_scale_changed)
-        self.geometry_holder.sigRingBoundsChanged.connect(self.roi_dict.change_ring_bounds)
-
-        self.fm.sigActiveImageChanged.connect(self.image_holder.change_image)
-        self.fm.sigProjectIsClosing.connect(self.save_state)
-
+        self.debug_tracker: TrackQObjects or None = None
         if self.log.level <= logging.DEBUG:
-            self.debug_tracker: TrackQObjects = TrackQObjects()
-        else:
-            self.debug_tracker: TrackQObjects = None
+            self.debug_tracker = TrackQObjects()
 
     def save_state(self):
         self.geometry_holder.save_state()
@@ -52,5 +47,20 @@ class App(metaclass=SingletonMeta):
     def geometry(self) -> Geometry:
         return self.geometry_holder.geometry
 
+    @property
+    def current_image_key(self) -> ImageKey or None:
+        return self.fm.current_key
+
     def close(self):
         self.fm.close()
+
+    def _connect_app(self):
+        self.image_holder.sigPolarImageChanged.connect(self.radial_profile.update)
+        self.image_holder.sigPolarImageChanged.connect(self.angular_profile.update)
+
+        self.geometry_holder.sigScaleChanged.connect(self.roi_dict.on_scale_changed)
+        self.geometry_holder.sigRingBoundsChanged.connect(self.roi_dict.change_ring_bounds)
+
+        self.fm.sigActiveImageChanged.connect(self.image_holder.change_image)
+        self.fm.sigProjectIsClosing.connect(self.save_state)
+        self.fm.sigActiveFolderChanged.connect(self.roi_dict.change_folder)
