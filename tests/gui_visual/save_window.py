692bea39a9eca4480295357cc3c709086f6e227a
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-10-02 11:10:00 +0200

    refactoring

--- tests/gui_visual/save_window.py
+++ tests/gui_visual/save_window.py
@@ -1,5 +1,5 @@
 from giwaxs_gui.gui.save_window import SaveWindow
-from tests.gui_visual.gui_wrapper import run_widget
+from tests.gui_visual.run_widget import run_widget
 from tests.fixures.projects import PROJECT_1_INFO
 
 
