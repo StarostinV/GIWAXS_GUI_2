44a348a976aa2687407c3daf13ca72778b3d8109
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-10-02 11:11:55 +0200

    updated Worker class

--- giwaxs_gui/app/utils.py
+++ giwaxs_gui/app/utils.py
@@ -13,87 +13,6 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-# class ChainProperties(object):
-#     """
-#     ChainProperties provides a functionality to simplify setting
-#     a choice of several options.
-#
-#     >>> class MyChainProperty(ChainProperties):
-#     ...     _PROPERTY_DICT = {'a': 0, 'b': 1, 'c': 2}
-#     ...
-#     >>>
-#     >>> my_chain_property = MyChainProperty()
-#     >>> my_chain_property.properties
-#     ()
-#     >>> my_chain_property.a
-#     MyChainProperty(a)
-#     >>> my_chain_property.b.c
-#     MyChainProperty(a, b, c)
-#     >>> my_chain_property.properties
-#     (0, 1, 2)
-#     >>> MyChainProperty().a.b.c
-#     MyChainProperty(a, b, c)
-#
-#     It can be initialized with a dictionary as a constructor parameter
-#     >>> another_chain_property = ChainProperties({'a': 0, 'b': 1, 'c': 2})
-#     >>> another_chain_property.a.properties
-#     (0,)
-#
-#     >>> another_chain_property.clear().properties
-#     ()
-#
-#     >>> len(MyChainProperty())
-#     0
-#     >>> len(MyChainProperty().a.b)
-#     2
-#     >>> bool(MyChainProperty())
-#     False
-#     >>> bool(MyChainProperty().a)
-#     True
-#     """
-#
-#     _PROPERTY_DICT = {}
-#     __slots__ = ('_chosen_params', '_chosen_values')
-#
-#     def __init__(self, params_dict: dict = None):
-#         if params_dict:
-#             self._PROPERTY_DICT.update(params_dict)
-#         self._chosen_params = []
-#         self._chosen_values = []
-#
-#         for prop in self._PROPERTY_DICT.keys():
-#             setattr(self.__class__, prop, property(self._prop_factory(prop)))
-#
-#     def _prop_factory(self, prop):
-#         def func(*args, **kwargs):
-#             if prop not in self._chosen_params:
-#                 self._chosen_params.append(prop)
-#                 self._chosen_values.append(self._PROPERTY_DICT[prop])
-#
-#             return self
-#
-#         return func
-#
-#     @property
-#     def properties(self):
-#         return tuple(self._chosen_values)
-#
-#     def __len__(self):
-#         return len(self._chosen_values)
-#
-#     def __bool__(self):
-#         return len(self) > 0
-#
-#     def __repr__(self):
-#         properties = ', '.join(self._chosen_params)
-#         return f'{self.__class__.__name__}({properties})'
-#
-#     def clear(self):
-#         self._chosen_params.clear()
-#         self._chosen_values.clear()
-#         return self
-
-
 class SingletonMeta(type):
     _instance = None
 
@@ -149,21 +68,22 @@ class WorkerSignals(QObject):
 class Worker(QRunnable):
     log = logging.getLogger(__name__)
 
-    def __init__(self, fn, *args, **kwargs):
+    def __init__(self, fn, *args, signals: WorkerSignals = None, **kwargs):
         super(Worker, self).__init__()
 
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
-        self.signals = WorkerSignals()
+        self.signals = signals or WorkerSignals()
+
+    def call_func(self):
+        return self.fn(*self.args, **self.kwargs)
 
     @pyqtSlot()
     def run(self):
 
         try:
-            result = self.fn(
-                *self.args, **self.kwargs
-            )
+            result = self.call_func()
         except Exception as err:
             self.log.exception(err)
             traceback.print_exc()
@@ -182,9 +102,3 @@ class Worker(QRunnable):
                 self.signals.finished.emit()
             except RuntimeError:
                 return
-
-#
-# if __name__ == "__main__":
-#     import doctest
-#
-#     doctest.testmod()
