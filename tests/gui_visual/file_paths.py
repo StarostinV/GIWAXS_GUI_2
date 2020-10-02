1cf31d8e976cd279e0bd4f5fdc81ed4d2998aabc
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2020-10-02 11:08:10 +0200

    refactoring

--- tests/gui_visual/file_paths.py
+++ tests/gui_visual/file_paths.py
@@ -1,5 +1,5 @@
 from giwaxs_gui.gui.save_window.path_line import PathLine, PathLineModes
-from tests.gui_visual.gui_wrapper import run_widget
+from tests.gui_visual.run_widget import run_widget
 
 
 if __name__ == '__main__':
